1c96134599e548b859eb8d40ac92cd1f3aef85d9
parithoshjenfvj <gudhiparithosh@gmail.com>
2026-04-07 23:46:25 +0530

    dashboard add item added

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -4,12 +4,9 @@ async function addItem(req,res){
     let {itemName,calories,protein,carbs,fiber,fat}=req.body
     let user=req.user
     let food=await foodModel.create({
-        userId:user._id,itemName,calories,protein,carbs,fiber,fat
+        userId:user._id,itemName,calories,protein,carbs,fiber,fat,date: new Date()
     })
-    res.status(201).json({
-        message:'Items added successfully',
-        food
-    })
+    res.redirect('/user')
 }
 async function getToday(req,res){
     const userId=req.user._id
